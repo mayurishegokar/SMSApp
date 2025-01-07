5c5796cdde7bc7db2d5166cfa543442e46831465
mayur <mayur@Mayuri>
2025-01-07 12:14:11 +0530

    Remove String variable in main class.

--- StudentManagementSystem/src/main/java/edu/sms/app/StudentManagementSystemApplication.java
+++ StudentManagementSystem/src/main/java/edu/sms/app/StudentManagementSystemApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class StudentManagementSystemApplication {
-       String ="Mayuri";
+       
 	public static void main(String[] args) {
 		SpringApplication.run(StudentManagementSystemApplication.class, args);
 	}
