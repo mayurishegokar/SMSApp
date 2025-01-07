a828f7f4ecf1eaae3f046b9146361de306dec686
mayurishegokar <mayurishegokar152@gmail.com>
2025-01-07 12:12:49 +0530

    Update StudentManagementSystemApplication.java

--- StudentManagementSystem/src/main/java/edu/sms/app/StudentManagementSystemApplication.java
+++ StudentManagementSystem/src/main/java/edu/sms/app/StudentManagementSystemApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class StudentManagementSystemApplication {
-int a;
+       String ="Mayuri";
 	public static void main(String[] args) {
 		SpringApplication.run(StudentManagementSystemApplication.class, args);
 	}
